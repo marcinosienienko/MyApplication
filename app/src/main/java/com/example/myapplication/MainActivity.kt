8fee9fe4e7c147855f75661bd52c4d1c6107ff0b
Marcin <marcinosienienko@gmail.com>
2023-07-20 20:20:20 +0200

    creating column and rows

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -32,7 +36,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
+/*@Composable
 private fun MyApp(modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier,
@@ -40,28 +44,47 @@ private fun MyApp(modifier: Modifier = Modifier) {
     ) {
         Greeting("Android")
     }
+} */
+
+@Composable
+fun MyApp(
+    modifier : Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for(name in names) {
+            Greeting(name = name)
+        }
+    }
 }
 
 
 @Composable
-private fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(
-                text = "Hello",
-                modifier = Modifier.padding(24.dp, 24.dp,24.dp,24.dp)
-            )
-            Text(
-                text = name,
-                modifier = Modifier.padding(24.dp, 24.dp,24.dp,24.dp)
-            )
+private fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp))
+    {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f))
+            {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+                ElevatedButton(onClick = { /*TODO*/ }) {
+                    Text(text = "Show more")
+                }
 
+            }
         }
+
     }
 
 
 
-}
+
+
+
 
 
 @Preview(showBackground = true)
